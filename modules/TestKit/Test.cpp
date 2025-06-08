043fc5b7a820e5b4c8a01aaf97ab6ae9792949ca
Sif Høg-Getgood <SHmusicus@mail.com>
2025-06-08 10:33:33 +0200

    Declared class Test

---
+++ modules/TestKit/Test.cpp
@@ -0,0 +1,29 @@
+module;
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+
+export module Test;
+
+export class Test
+{
+public:
+  Test(std::string a_name, const std::set<std::string> &a_features);
+  explicit Test(std::string a_name);
+  void AddFeatureTest(std::string a_description, std::set<std::string> a_testedFeatures,
+                      std::function<void()> a_testLogic);
+  Test &AddFeature(std::string a_feature);
+  void Run();
+
+private:
+  std::vector<FeatureTest> m_featureTests;
+  std::string m_name;
+  std::map<std::string, TestCompletion> m_testCompletions;
+};
+
+Test::Test(std::string a_name, const std::set<std::string> &a_features) : m_name(std::move(a_name))
+{
+  std::cout << a_features.size() << std::endl; // TODO, handle a_features
+};
+Test::Test(std::string a_name) : m_name(std::move(a_name)) {}
