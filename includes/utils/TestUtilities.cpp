90d37ece8d0bac3e43c6599e7b2b26eae3d7e5b9
Sif Høg-Getgood <dev@hg-mail.eu>
2025-07-20 22:01:53 +0200

    Declared and defined TestUtilities::MeasureExecutionTime

--- includes/utils/TestUtilities.cpp
+++ includes/utils/TestUtilities.cpp
@@ -1,14 +1,34 @@
 #include "TestUtilities.hpp"
-#include <iostream>
+#include <chrono>
 #include <functional>
+#include <iostream>
 
 void TestUtilities::LogInfo(const std::string &a_message) {
     std::cout << "\033[1;36m" << a_message << "\033[0m" << std::endl;
+}
+
+std::chrono::duration<double, std::milli> TestUtilities::MeasureExecutionTime(
+  const std::function<void()> &a_action
+)
+{
+  auto start = std::chrono::high_resolution_clock::now();
+  a_action();
+  auto end = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration<double, std::milli>(end - start);
 }
 
 bool TestUtilities::TryExecute(
   const std::string &a_actionDescription,
   const std::function<void()> &a_action)
 {
+  try
+  {
+    /* code */
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
   return true;
 }
--- includes/utils/TestUtilities.hpp
+++ includes/utils/TestUtilities.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
-#include <string>
+#include <chrono>
 #include <functional>
+#include <string>
 
 class TestUtilities
 {
 public:
   static void LogInfo(const std::string &a_message);
+  static std::chrono::duration<double, std::milli> MeasureExecutionTime(
+    const std::function<void()> &a_action
+  );
   static bool TryExecute(
     const std::string &a_actionDescription,
     const std::function<void()> &a_action);
