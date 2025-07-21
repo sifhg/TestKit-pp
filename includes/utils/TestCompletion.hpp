fa493b583c56f0e11717e62e2ff6bb29abb4057d
Sif Høg-Getgood <SHmusicus@mail.com>
2025-07-21 21:01:22 +0200

    Add evaluation methods to TestCompletion

--- includes/utils/TestCompletion.hpp
+++ includes/utils/TestCompletion.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <iomanip>
+#include <sstream>
 
 struct TestCompletion
 {
@@ -10,4 +12,22 @@ struct TestCompletion
     passedTests++;
   }
   void Fail() { totalTests++; }
+  [[nodiscard]] std::string EvaluateTest() const
+  {
+    if (totalTests == 0)
+    {
+      return "(no tests run)";
+    }
+    std::ostringstream percentageStream;
+    percentageStream << std::fixed << std::setprecision(2)
+                     << static_cast<float>(passedTests) / static_cast<float>(totalTests) * 100;
+
+    return percentageStream.str() + "\n" + std::to_string(passedTests) + " out of " +
+           std::to_string(totalTests) + " tests passed";
+  }
+  [[nodiscard]] bool DidAllTestsPass() const
+  {
+    return totalTests != 0 && passedTests == totalTests;
+  }
+  [[nodiscard]] bool DidAnyTestPass() const { return passedTests != 0; }
 };
