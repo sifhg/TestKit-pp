a125cd587f805548ce27a27d8edf8968a156b887
Sif Høg-Getgood <SHmusicus@mail.com>
2025-07-21 20:25:56 +0200

    Declare FeatureTest struct

--- includes/utils/FeatureTest.hpp
+++ includes/utils/FeatureTest.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include <functional>
+#include <string>
+#include <vector>
 
 struct FeatureTest
 {
-    /* data */
+  std::string description;
+  std::vector<std::string> testedFeatures;
+  std::function<void()> testLogic;
 };
