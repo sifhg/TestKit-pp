6522ed3c25f93d74d4b75e6fbccc72b1a80ae90e
Sif Høg-Getgood <SHmusicus@mail.com>
2025-07-21 20:04:24 +0200

    Update main.cpp

--- test_executable/main.cpp
+++ test_executable/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <list>
 #include "../includes/Test.hpp"
-// import Test;
 
 int main()
 {
-  Test aTest = Test("I'm a test", {"one", "two", "three"});
+  auto aTest = Test("I'm a test", {"one", "two", "three"});
   std::cout << aTest.to_string() << ".\n";
-  aTest.AddFeature("Two")
-    .AddFeature("Three");
+  aTest.AddFeature("Two").AddFeature("Three");
   std::cout << aTest.to_string() << ".\n";
   aTest.Run();
   // Test aTest = Test("I'm a test", {"Volvo", "BMW", "Ford", "Mazda"});
