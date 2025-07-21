295458c4c9d8598f5a18df3529362bf7204181d3
Sif Høg-Getgood <SHmusicus@mail.com>
2025-07-21 21:09:42 +0200

    Update main.cpp

--- test_executable/main.cpp
+++ test_executable/main.cpp
@@ -8,9 +8,5 @@ int main()
   aTest.AddFeature("Two").AddFeature("Three");
   std::cout << aTest.to_string() << ".\n";
   aTest.Run();
-  // Test aTest = Test("I'm a test", {"Volvo", "BMW", "Ford", "Mazda"});
-  // aTest.AddFeature("Heej").AddFeature("Heejj").AddFeature("Heejjj");
-  // aTest.Run();
-  // std::cout << "Press enter to end...\n";
   return 0;
 }
